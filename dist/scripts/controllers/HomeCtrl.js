042549f9cfadb8500ea7b3bdf1d8b8bd21a60db3
Alissa Likavec <alissalikavec@gmail.com>
2017-01-06 12:06:24 -0800

    Firebase fix

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,21 +1,28 @@
 (function(){
   function HomeCtrl($firebaseArray){
     // TODO: set up firebase
-    // var ref = firebase.database().ref().child("current-todos");
+    var ref = firebase.database().ref().child("active");
 
-    // this.data = $firebaseArray(ref);
-    this.data = [{
-      name: "Task #1",
-      completed: true
-    },
-    {
-      name: "Task #2",
-      completed: false
-    },
-    {
-      name: "Task #3",
-      completed: false
-    }]
+    this.data = $firebaseArray(ref);
+    // this.data = [{
+    //   name: "Task #1",
+    //   createdAt: new Date(),
+    //   completed: true,
+    //   expired: false
+    // },
+    // {
+    //   name: "Task #2",
+    //   createdAt: new Date(),
+    //   completed: false,
+    //   expired: true
+    // },
+    // {
+    //   name: "Task #3",
+    //   completed: false
+    //   createdAt: new Date(),
+    //   completed: false,
+    //   expired: false
+    // }]
     this.myHomeCtrlArray = [1, 2, 3, 4, 5];
   }
 
